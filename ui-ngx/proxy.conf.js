f6a2958fafe2a46b8aef1a475a48c3629d1fb4c4
PhiVietHoang <111297358+PhiVietHoang@users.noreply.github.com>
2024-11-11 08:50:03 +0700

    Update proxy.conf.js

--- ui-ngx/proxy.conf.js
+++ ui-ngx/proxy.conf.js
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 const forwardUrl = "http://localhost:8080";
-const wsForwardUrl = "ws://localhost:8080";
+const wsForwardUrl = "ws://222.252.25.37:9999";
 const ruleNodeUiforwardUrl = forwardUrl;
 
 const PROXY_CONFIG = {
