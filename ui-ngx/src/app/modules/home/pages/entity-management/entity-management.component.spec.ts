393d3b8f522167fe8c974d3d968f1817ab2afa78
PhiVietHoang <111297358+PhiVietHoang@users.noreply.github.com>
2024-12-02 11:23:31 +0700

    Update entity-management.component.spec.ts

--- ui-ngx/src/app/modules/home/pages/entity-management/entity-management.component.spec.ts
+++ ui-ngx/src/app/modules/home/pages/entity-management/entity-management.component.spec.ts
@@ -13,6 +13,7 @@
 /// See the License for the specific language governing permissions and
 /// limitations under the License.
 ///
+
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { EntityManagementComponent } from './entity-management.component';
